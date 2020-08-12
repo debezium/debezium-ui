4d36905eb0325c93add3a3627c1189c2c3aeaf44
indraraj <indraraj14@gmail.com>
2020-08-12 16:10:35 +0530

    Add ErrorBoundary and other error page with loader

--- ui/packages/ui/src/app/pages/createConnection/CreateConnectionPage.tsx
+++ ui/packages/ui/src/app/pages/createConnection/CreateConnectionPage.tsx
@@ -1,49 +1,64 @@
 import React from "react";
-import { Breadcrumb, 
-         BreadcrumbItem, 
-         PageSection, 
-         PageSectionVariants, 
-         Wizard } from '@patternfly/react-core';
-import '../../app.css';
-import { SelectConnectorComponent } from './index'
-import { Services } from "@debezium/ui-services";
-import { ConnectorType } from "@debezium/ui-models";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  PageSection,
+  PageSectionVariants,
+  Wizard,
+} from "@patternfly/react-core";
+import { SelectConnectorComponent } from "./connectionSteps";
+import './CreateConnectionPage.css'
 
 export const CreateConnectionPage: React.FunctionComponent = () => {
-  
   const [stepIdReached, setStepIdReached] = React.useState(1);
-  const [connectorTypes, setConnectorTypes] = React.useState<ConnectorType[]>([]);
 
   const onFinish = () => {
     // TODO: Validate the connector entries.  Redirect to connections upon success, otherwise stay on page.
     // history.push('/connections');
-    alert('wizard finish');
+    alert("wizard finish");
   };
 
   const onCancel = () => {
     // history.push('/connections');
-    alert('wizard cancel');
+    alert("wizard cancel");
   };
 
-  const onNext = ({ id }) => {
+  const onNext = ({ id }: any) => {
     setStepIdReached(stepIdReached < id ? id : stepIdReached);
   };
 
   const wizardSteps = [
-    { id: 1, name: 'Select Connector', component: <SelectConnectorComponent connectorTypes={connectorTypes} /> },
-    { id: 2, name: 'Configure Connector', component: <p>component for connector config</p>, canJumpTo: stepIdReached >= 2 },
-    { id: 3, name: 'Select Tables', component: <p>component for table selection</p>, canJumpTo: stepIdReached >= 3 },
-    { id: 4, name: 'Data Options', component: <p>component for data options</p>, canJumpTo: stepIdReached >= 4 },
-    { id: 5, name: 'Set Target', component: <p>component for set target</p>, canJumpTo: stepIdReached >= 5, nextButtonText: 'Finish' }
+    {
+      id: 1,
+      name: "Select Connector",
+      component: <SelectConnectorComponent />,
+    },
+    {
+      id: 2,
+      name: "Configure Connector",
+      component: <p>component for connector config</p>,
+      canJumpTo: stepIdReached >= 2,
+    },
+    {
+      id: 3,
+      name: "Select Tables",
+      component: <p>component for table selection</p>,
+      canJumpTo: stepIdReached >= 3,
+    },
+    {
+      id: 4,
+      name: "Data Options",
+      component: <p>component for data options</p>,
+      canJumpTo: stepIdReached >= 4,
+    },
+    {
+      id: 5,
+      name: "Set Target",
+      component: <p>component for set target</p>,
+      canJumpTo: stepIdReached >= 5,
+      nextButtonText: "Finish",
+    },
   ];
-
-  React.useEffect(() => {
-    Services.getGlobalsService()
-      .getConnectorTypes()
-      .then((cTypes: React.SetStateAction<ConnectorType[]>) => {
-        setConnectorTypes(cTypes);
-      });
-  }, [setConnectorTypes]);
 
   return (
     <>
@@ -66,6 +81,7 @@ export const CreateConnectionPage: React.FunctionComponent = () => {
           onSave={onFinish}
           steps={wizardSteps}
           height={400}
+          className="create-connection-page_wizard"
         />
       </PageSection>
     </>
