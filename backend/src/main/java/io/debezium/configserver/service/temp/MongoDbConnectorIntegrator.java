b07ee1ca9149f2ce8fc9e93fe9a2f37ba95b406b
Gunnar Morling <gunnar.morling@googlemail.com>
2020-08-12 10:24:14 +0200

    Dummy integrators for more connectors

---
+++ backend/src/main/java/io/debezium/configserver/service/temp/MongoDbConnectorIntegrator.java
@@ -0,0 +1,28 @@
+package io.debezium.configserver.service.temp;
+
+import java.util.Map;
+
+import org.apache.kafka.connect.source.SourceConnector;
+
+import io.debezium.configserver.model.FilterValidationResult;
+import io.debezium.configserver.service.ConnectorIntegratorBase;
+import io.debezium.connector.mongodb.MongoDbConnector;
+
+// TODO: This will live in the actual connector module eventually
+public class MongoDbConnectorIntegrator extends ConnectorIntegratorBase {
+
+    @Override
+    public FilterValidationResult validateFilters(Map<String, String> properties) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    protected ConnectorDescriptor getConnectorDescriptor() {
+        return new ConnectorDescriptor("mongodb", "MongoDB", false);
+    }
+
+    @Override
+    protected SourceConnector getConnector() {
+        return new MongoDbConnector();
+    }
+}
---
+++ backend/src/main/java/io/debezium/configserver/service/temp/MySqlConnectorIntegrator.java
@@ -0,0 +1,28 @@
+package io.debezium.configserver.service.temp;
+
+import java.util.Map;
+
+import org.apache.kafka.connect.source.SourceConnector;
+
+import io.debezium.configserver.model.FilterValidationResult;
+import io.debezium.configserver.service.ConnectorIntegratorBase;
+import io.debezium.connector.mysql.MySqlConnector;
+
+// TODO: This will live in the actual connector module eventually
+public class MySqlConnectorIntegrator extends ConnectorIntegratorBase {
+
+    @Override
+    public FilterValidationResult validateFilters(Map<String, String> properties) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    protected ConnectorDescriptor getConnectorDescriptor() {
+        return new ConnectorDescriptor("mysql", "MySQL", false);
+    }
+
+    @Override
+    protected SourceConnector getConnector() {
+        return new MySqlConnector();
+    }
+}
