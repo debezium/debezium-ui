303a116d123b0c4152c5a5e99a98249aa35ff29b
Hussain Ansari <nasiba.ansari@gmail.com>
2020-08-19 19:13:01 +0530

    layout wrapper as per UX (#25)
    * fixing spacing issue
    
    * Webpack dev config revert
    
    * fixing app conflict
    
    * spacing issue
    
    Co-authored-by: uidoyen <asansari@localhost.localdomain>

---
+++ ui/packages/ui/src/app/Layout/AppLayout.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './layout.css';
+import {
+	Page,
+	PageSection,
+	PageSectionVariants,
+	SkipToContent,
+} from '@patternfly/react-core';
+
+import AppHeader from './appHeader';
+import { AppLayoutContext } from './AppLayoutContext';
+
+const AppLayout: React.FC = ({children}) => {
+
+	const [breadcrumb, setHasBreadcrumb] = React.useState(null);
+  	const showBreadcrumb = (b: any) => setHasBreadcrumb(b);
+
+	const PageSkipToContent = (
+		<SkipToContent href="#main-content-page-layout-default-nav">
+			Skip to Content
+		</SkipToContent>
+	);
+
+	return (
+		<AppLayoutContext.Provider value={{ showBreadcrumb }}>
+			<Page
+				header={<AppHeader />}
+				skipToContent={PageSkipToContent}
+				breadcrumb={breadcrumb}
+				className="app-page"
+			>
+				<PageSection variant={PageSectionVariants.light}>
+					{children}
+				</PageSection>
+			</Page>
+		</AppLayoutContext.Provider>
+	)
+}
+
+export default AppLayout;
---
+++ ui/packages/ui/src/app/Layout/Breadcrumb.tsx
@@ -0,0 +1,48 @@
+import {
+	Breadcrumb as PFBreadcrumb,
+	BreadcrumbItem,
+	BreadcrumbItemProps,
+	Level,
+	LevelItem,
+  } from '@patternfly/react-core';
+  import * as React from 'react';
+  import { AppBreadcrumb } from './AppBreadcrumb';
+  
+  export { BreadcrumbItem, BreadcrumbItemProps };
+  
+  export interface IBreadcrumbProps {
+	items?: Array<React.ReactElement<BreadcrumbItemProps>>;
+	actions?: React.ReactNode;
+  }
+  
+  /**
+   * A component to show breadcrumbs. All its children will be wrapped in a tag
+   * that will automatically handle the active/inactive state by setting the
+   * appropriate class to the wrapper.
+   *
+   * It's suggested to use only anchors or spans as children node.
+   */
+  export const Breadcrumb: React.FunctionComponent<IBreadcrumbProps> = ({
+	items,
+	actions,
+	children,
+  }) => {
+	items =
+	  items ||
+	  React.Children.toArray(children).map((c, idx) => (
+		<BreadcrumbItem key={idx} isActive={idx === count - 1}>
+		  {c}
+		</BreadcrumbItem>
+	  ));
+	const count = items.length;
+	return (
+	  <AppBreadcrumb>
+		<Level gutter={'md'}>
+		  <LevelItem>
+			<PFBreadcrumb>{items}</PFBreadcrumb>
+		  </LevelItem>
+		  {actions && <LevelItem>{actions}</LevelItem>}
+		</Level>
+	  </AppBreadcrumb>
+	);
+  };
