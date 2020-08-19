4e79cdd0b43c8e3f3c948b3d33f5fe8d789d97b7
Indra <indraraj14@gmail.com>
2020-08-19 19:34:08 +0530

    Lifting connector-type api call to createConnnectorPage (#27)
    Co-authored-by: Indra Shukla <ishukla@localhost.localdomain>

--- ui/packages/ui/src/app/pages/createConnector/connectorSteps/SelectConnectorTypeComponent.tsx
+++ ui/packages/ui/src/app/pages/createConnector/connectorSteps/SelectConnectorTypeComponent.tsx
@@ -9,101 +9,61 @@ import {
   CardBody,
 } from "@patternfly/react-core";
 import "./SelectConnectorTypeComponent.css";
-import { Services } from "@debezium/ui-services";
 import { ConnectorType } from "@debezium/ui-models";
 import { DatabaseIcon } from "@patternfly/react-icons";
-// import { ConnectorType } from "@debezium/ui-models";
-import {
-  getConnectorTypeDescription,
-  fetch_retry,
-} from "../../../shared/Utils";
+import { getConnectorTypeDescription } from "../../../shared/Utils";
 import { WithLoader } from "src/app/shared/WithLoader";
 import { ApiError } from "src/app/shared";
 import { PageLoader } from "src/app/component";
 
-// Put the enabled types first, then the disabled types.  alpha sort each group
-function getSortedConnectorTypes(connectorTypes: ConnectorType[]) {
-  const enabledTypes: ConnectorType[] = connectorTypes
-    .filter((cType) => cType.enabled)
-    .sort((thisType, thatType) => {
-      return thisType.displayName.localeCompare(thatType.displayName);
-    });
-
-  const disabledTypes: ConnectorType[] = connectorTypes
-    .filter((cType) => !cType.enabled)
-    .sort((thisType, thatType) => {
-      return thisType.displayName.localeCompare(thatType.displayName);
-    });
-
-  return [...enabledTypes, ...disabledTypes];
-}
-
-// tslint:disable-next-line: no-empty-interface
 export interface ISelectConnectorTypeComponentProps {
-  initialSelection?: string;
+  selectedConnectorType?: string;
   onSelectionChange: (connectorType: string | undefined) => Promise<void>;
+  connectorTypesList: ConnectorType[];
+  loading: boolean;
+  apiError: boolean;
+  errorMsg: Error;
 }
 
 export const SelectConnectorTypeComponent: React.FunctionComponent<ISelectConnectorTypeComponentProps> = (
   props
 ) => {
-  const [connectorTypes, setConnectorTypes] = React.useState<ConnectorType[]>(
-    []
-  );
-  const [selectedType, setSelectedType] = React.useState<string | undefined>(
-    props.initialSelection
-  );
-
-  const [loading, setLoading] = React.useState(true);
-  const [apiError, setApiError] = React.useState<boolean>(false);
-  const [errorMsg, setErrorMsg] = React.useState<Error>();
-
-  React.useEffect(() => {
-    const globalsService = Services.getGlobalsService();
-    fetch_retry(globalsService.getConnectorTypes, globalsService)
-      .then((cTypes: React.SetStateAction<ConnectorType[]>) => {
-        setLoading(false);
-        setConnectorTypes(cTypes);
-      })
-      .catch((err: React.SetStateAction<Error | undefined>) => {
-        setApiError(true);
-        setErrorMsg(err);
-      });
-  }, [setConnectorTypes]);
-
-  const onClick = (event: { currentTarget: { id: string } }) => {
+  const onCardSelection = (event: { currentTarget: { id: string } }) => {
     // The id is the connector className
     const newId = event.currentTarget.id;
-    const selectedConn = connectorTypes.find(
+    const selectedConn = props.connectorTypesList.find(
       (cType) => cType.className === newId
     );
 
     // Set selection undefined if connector is not enabled or no change in type (deselection)
     const newSelection =
-      !selectedConn!.enabled || selectedType === newId ? undefined : newId;
-    setSelectedType(newSelection);
+      !selectedConn!.enabled || props.selectedConnectorType === newId
+        ? undefined
+        : newId;
     props.onSelectionChange(newSelection);
   };
 
   return (
     <WithLoader
-      error={apiError}
-      loading={loading}
+      error={props.apiError}
+      loading={props.loading}
       loaderChildren={<PageLoader />}
-      errorChildren={<ApiError error={errorMsg!} />}
+      errorChildren={<ApiError error={props.errorMsg} />}
     >
       {() => (
         <Flex>
-          {getSortedConnectorTypes(connectorTypes).map((cType, index) => (
+          {props.connectorTypesList.map((cType, index) => (
             <FlexItem
               key={index}
               className={"select-connector-type-component_cardItem"}
             >
               <Card
                 id={cType.className}
-                onClick={onClick}
+                // tslint:disable-next-line: no-empty
+                onClick={cType.enabled ? onCardSelection : ()=>{}}
                 isSelectable={cType.enabled}
-                isSelected={selectedType === cType.className}
+                isSelected={props.selectedConnectorType === cType.className}
+                className={!cType.enabled ? 'select-connector-type-component_disableCard' : ''}
               >
                 <CardHeader>
                   <CardHeaderMain
