b6aaf46b9aa7c4fa47f98c9eb760d25df3cfd4b2
Indra <indraraj14@gmail.com>
2020-08-20 21:29:08 +0530

    adding DBZ favicon (#30)
    Co-authored-by: Indra Shukla <ishukla@localhost.localdomain>

--- ui/packages/ui/webpack.common.js
+++ ui/packages/ui/webpack.common.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
     })
   ],
   module: {
