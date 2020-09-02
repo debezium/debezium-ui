fd1502f48004fbd7dbc46397c9853b4931b37835
rkerner <rkerner.mobil@gmail.com>
2020-09-02 17:33:56 +0200

    * minor fix addressing PR feedback

--- backend/src/test/java/io/debezium/configserver/util/PostgresInfrastructureTestProfile.java
+++ backend/src/test/java/io/debezium/configserver/util/PostgresInfrastructureTestProfile.java
@@ -2,17 +2,12 @@ package io.debezium.configserver.util;
 
 import io.quarkus.test.junit.QuarkusTestProfile;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 
 public class PostgresInfrastructureTestProfile implements QuarkusTestProfile {
     @Override
     public List<TestResourceEntry> testResources() {
-        ArrayList<TestResourceEntry> resourceEntries = new ArrayList<>(1) {};
-        resourceEntries.add(new TestResourceEntry(PostgresInfrastructureTestResourceLifecycleManager.class));
-        return resourceEntries;
+        return Collections.singletonList(new TestResourceEntry(PostgresInfrastructureTestResourceLifecycleManager.class));
     }
 }
-
-
