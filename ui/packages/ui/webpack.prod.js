faf491d388755690ebd12df54736151b3f3b33ed
Mark Drilling <mdrillin@redhat.com>
2020-12-10 05:15:25 -0600

    Converts connector list to patternfly table (#226)
    LGTM.

--- ui/packages/ui/webpack.prod.js
+++ ui/packages/ui/webpack.prod.js
@@ -26,9 +26,11 @@ module.exports = merge(common, {
           path.resolve(__dirname, 'src'),
           path.resolve(__dirname, 'node_modules/patternfly'),
           path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css')
+          path.resolve(__dirname, '../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
         ],
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
