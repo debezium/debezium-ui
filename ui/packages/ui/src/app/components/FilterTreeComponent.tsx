ae7696cb228a8523ad9980f84b89d5dd2678c050
indraraj <indraraj14@gmail.com>
2021-01-28 12:18:57 +0530

    alert for column filter

--- ui/packages/ui/src/app/components/FilterTreeComponent.tsx
+++ ui/packages/ui/src/app/components/FilterTreeComponent.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   EmptyState,
   EmptyStateBody,
   EmptyStateIcon,
@@ -18,18 +19,31 @@ export interface IFilterTreeComponentProps {
   loading: boolean;
   apiError: boolean;
   errorMsg: Error;
-  invalidMsg: Map<string,string> | undefined;
+  columnOrFieldFilter: string;
+  invalidMsg: Map<string, string> | undefined;
+  childNo: number;
+  filterValues: Map<string, string>;
+  clearFilter: () => void;
   i18nApiErrorTitle?: string;
   i18nApiErrorMsg?: string;
   i18nNoMatchingTables: string;
-  i18nNoMatchingFilterExpMsg: string;
   i18nInvalidFilters: string;
   i18nInvalidFilterExpText: string;
+  i18nInvalidFilterText: string;
+  i18nMatchingFilterExpMsg: string;
+  i18nNoMatchingFilterExpMsg: string;
+  i18nClearFilterText: string;
+  i18nClearFilters: string;
+  i18nFilterExpressionResultText: string;
 }
 export const FilterTreeComponent: React.FunctionComponent<IFilterTreeComponentProps> = (
   props
 ) => {
   const [activeItems, setActiveItems] = React.useState<any>();
+
+  const getColumnOrFieldMsg = () =>{
+    return props.columnOrFieldFilter ? `. ${props.columnOrFieldFilter}(s) name not available in preview but the ${props.columnOrFieldFilter} filter expression will be applied in created connector.` : ''
+  }
 
   const onClick = (evt: any, treeViewItem: any, parentItem: any) => {
     setActiveItems([treeViewItem, parentItem]);
@@ -39,37 +53,80 @@ export const FilterTreeComponent: React.FunctionComponent<IFilterTreeComponentPr
       error={props.apiError}
       loading={props.loading}
       loaderChildren={<PageLoader />}
-      errorChildren={<ApiError i18nErrorTitle={props.i18nApiErrorTitle} i18nErrorMsg={props.i18nApiErrorMsg} error={props.errorMsg} />}
+      errorChildren={
+        <ApiError
+          i18nErrorTitle={props.i18nApiErrorTitle}
+          i18nErrorMsg={props.i18nApiErrorMsg}
+          error={props.errorMsg}
+        />
+      }
     >
       {() =>
-        props.treeData.length === 0 ? props.invalidMsg?.size === 0 ?(
-          <EmptyState variant={EmptyStateVariant.small}>
-            <EmptyStateIcon icon={CubesIcon} />
-            <Title headingLevel="h4" size="lg">
-              {props.i18nNoMatchingTables}
-            </Title>
-            <EmptyStateBody className="filter-tree-component_emptyBody">
-              {props.i18nNoMatchingFilterExpMsg}
-            </EmptyStateBody>
-          </EmptyState>
+        props.treeData.length === 0 ? (
+          props.invalidMsg?.size === 0 ? (
+            <>
+              <Alert
+                variant={"warning"}
+                isInline={true}
+                title={props.i18nNoMatchingFilterExpMsg + getColumnOrFieldMsg()}
+              >
+                <p>
+                  {props.i18nClearFilterText + " "}
+                  <a onClick={props.clearFilter}>{props.i18nClearFilters}</a>
+                </p>
+              </Alert>
+              <EmptyState variant={EmptyStateVariant.small}>
+                <EmptyStateIcon icon={CubesIcon} />
+                <Title headingLevel="h4" size="lg">
+                  {props.i18nNoMatchingTables}
+                </Title>
+                <EmptyStateBody className="filter-tree-component_emptyBody">
+                  {props.i18nNoMatchingFilterExpMsg}
+                </EmptyStateBody>
+              </EmptyState>
+            </>
+          ) : (
+            <>
+              <Alert
+                variant={"danger"}
+                isInline={true}
+                title={props.i18nInvalidFilterText}
+              />
+              <EmptyState variant={EmptyStateVariant.small}>
+                <EmptyStateIcon icon={ExclamationCircleIcon} />
+                <Title headingLevel="h4" size="lg">
+                  {props.i18nInvalidFilters}
+                </Title>
+                <EmptyStateBody className="filter-tree-component_emptyBody">
+                  {props.i18nInvalidFilterExpText}
+                </EmptyStateBody>
+              </EmptyState>
+            </>
+          )
         ) : (
-          <EmptyState variant={EmptyStateVariant.small}>
-            <EmptyStateIcon icon={ExclamationCircleIcon} />
-            <Title headingLevel="h4" size="lg">
-              {props.i18nInvalidFilters}
-            </Title>
-            <EmptyStateBody className="filter-tree-component_emptyBody">
-              {props.i18nInvalidFilterExpText}
-            </EmptyStateBody>
-          </EmptyState>
-        ) : (
-          <TreeView
-            data={props.treeData}
-            activeItems={activeItems}
-            onSelect={onClick}
-            hasBadges={true}
-          />
-          
+          <>
+            <Alert
+              variant={"info"}
+              isInline={true}
+              title={`${props.childNo} ${props.i18nMatchingFilterExpMsg}` + getColumnOrFieldMsg()}
+            >
+              {props.filterValues.size !== 0 ? (
+                <p>
+                  {props.i18nClearFilterText + " "}
+                  <a onClick={props.clearFilter}>{props.i18nClearFilters}</a>
+                </p>
+              ) : (
+                ""
+              )}
+            </Alert>
+
+            <TreeView
+              data={props.treeData}
+              activeItems={activeItems}
+              onSelect={onClick}
+              hasBadges={true}
+            />
+          </>
         )
       }
     </WithLoader>
