cdb988177580ce083fa28016516be48eeebfae45
Hussain Ansari <nasiba.ansari@gmail.com>
2021-02-17 17:56:19 +0530

    DBZ-3125 Fix production build issue (#323)

--- ui/packages/ui/webpack.prod.js
+++ ui/packages/ui/webpack.prod.js
@@ -1,16 +1,18 @@
 const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
   optimization: {
     minimizer: [
       new OptimizeCSSAssetsPlugin({})
     ]
+  },
+  output: {
+    filename: '[name].bundle.[contenthash].js'
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -29,38 +31,28 @@ module.exports = merge(common, {
           path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, '../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css')
         ],
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: {importLoaders: 1},
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            config: {
-              path: __dirname + '/postcss.config.js'
-            }
+        // use: ["style-loader", "css-loader", "postcss-loader"]
+        use: [
+          {
+            loader: 'style-loader'
           },
-        },]
+          {
+            loader: 'css-loader',
+            options: {importLoaders: 1},
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: __dirname + '/postcss.config.js'
+              }
+            },
+          },
+        ],
       }
     ]
-  },
-  output: {
-    filename: '[name].bundle.[contenthash].js'
-  },
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
   }
 });
