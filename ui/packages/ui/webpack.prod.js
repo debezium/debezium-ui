f3d65e0b6dd53bd99b9761f1606081d879167039
uidoyen <nasiba.ansari@gmail.com>
2021-03-10 18:41:32 +0530

    add css-minimizer-webpack-plugin for Webpack 5

--- ui/packages/ui/webpack.prod.js
+++ ui/packages/ui/webpack.prod.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
   optimization: {
+    minimize: true,
     minimizer: [
-      new OptimizeCSSAssetsPlugin({})
+      new CssMinimizerPlugin()
     ]
   },
   output: {
