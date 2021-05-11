323d25ca096445b71c6e7022ce5a4e8a73cef676
indraraj <indraraj14@gmail.com>
2021-05-11 15:35:22 +0530

    removing the skip/not used/

--- ui/packages/ui/src/app/Layout/AppLayout.tsx
+++ ui/packages/ui/src/app/Layout/AppLayout.tsx
@@ -1,42 +1,30 @@
-import {
-	Page,
-	PageSection,
-	PageSectionVariants,
-	SkipToContent
-} from '@patternfly/react-core';
-import * as React from 'react';
-import AppHeader from './appHeader';
-import { AppLayoutContext } from './AppLayoutContext';
-import './layout.css';
+import { Page, PageSection, PageSectionVariants } from "@patternfly/react-core";
+import * as React from "react";
+import AppHeader from "./appHeader";
+import { AppLayoutContext } from "./AppLayoutContext";
+import "./layout.css";
 
 const AppLayout: React.FC = ({ children }) => {
-	const [breadcrumb, setHasBreadcrumb] = React.useState(null);
-	const [clusterId, setClusterId] = React.useState<number>(1);
-	const showBreadcrumb = (b: any) => setHasBreadcrumb(b);
+  const [breadcrumb, setHasBreadcrumb] = React.useState(null);
+  const [clusterId, setClusterId] = React.useState<number>(1);
+  const showBreadcrumb = (b: any) => setHasBreadcrumb(b);
 
-	const PageSkipToContent = (
-		<SkipToContent href="#main-content-page-layout-default-nav" style={{display:'none'}}>
-			Skip to Content
-		</SkipToContent>
-	);
-	const handleClusterIdChange = (id: number) => {
-		setClusterId(id);
-	};
-	return (
-
-		<AppLayoutContext.Provider value={{ showBreadcrumb, clusterId }}>
-			<Page
-				header={<AppHeader handleClusterChange={handleClusterIdChange} />}
-				skipToContent={PageSkipToContent}
-				breadcrumb={breadcrumb}
-				className="app-page"
-			>
-				<PageSection variant={PageSectionVariants.light}>
-					{children}
-				</PageSection>
-			</Page>
-		</AppLayoutContext.Provider>
-	)
-}
+  const handleClusterIdChange = (id: number) => {
+    setClusterId(id);
+  };
+  return (
+    <AppLayoutContext.Provider value={{ showBreadcrumb, clusterId }}>
+      <Page
+        header={<AppHeader handleClusterChange={handleClusterIdChange} />}
+        breadcrumb={breadcrumb}
+        className="app-page"
+      >
+        <PageSection variant={PageSectionVariants.light}>
+          {children}
+        </PageSection>
+      </Page>
+    </AppLayoutContext.Provider>
+  );
+};
 
 export default AppLayout;
