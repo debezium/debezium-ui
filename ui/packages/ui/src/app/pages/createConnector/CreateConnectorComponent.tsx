52b0159397a15286b5bf59bd58c6d99aa6cf6c33
Mark Drilling <mdrillin@redhat.com>
2021-11-16 12:05:18 -0600

    DBZ-4293 Corrects regression in previous commit

--- ui/packages/ui/src/app/pages/createConnector/CreateConnectorComponent.tsx
+++ ui/packages/ui/src/app/pages/createConnector/CreateConnectorComponent.tsx
@@ -542,9 +542,8 @@ export const CreateConnectorComponent: React.FunctionComponent<ICreateConnectorC
   // Allows conversion of map keys, e.g. between dotted and underscore delimited forms
   const convertPropertyKeys = (propertyMap: Map<string, string>, searchStr: string, replaceStr: string) => {
     const convertedMap = new Map<string, string>();
-    const regEx = new RegExp(searchStr, 'g');
     for (const [key, value] of propertyMap) {
-      convertedMap.set(key.replace(regEx,replaceStr), value);
+      convertedMap.set(key.split(searchStr).join(replaceStr), value);
     }
     return convertedMap; 
   }
