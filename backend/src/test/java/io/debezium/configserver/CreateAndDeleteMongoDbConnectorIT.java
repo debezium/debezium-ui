1d4ac2be0cd41f90f32bd150b57248de4a469593
Chris Cranford <chris@hibernate.org>
2023-05-01 11:32:35 -0400

    DBZ-6363 Re-enable MongoDB tests

--- backend/src/test/java/io/debezium/configserver/CreateAndDeleteMongoDbConnectorIT.java
+++ backend/src/test/java/io/debezium/configserver/CreateAndDeleteMongoDbConnectorIT.java
@@ -14,7 +14,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -24,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 @TestProfile(MongoDbInfrastructureTestProfile.class)
-@Disabled("Temporarily disabled MongoDB based tests")
 public class CreateAndDeleteMongoDbConnectorIT {
 
     @BeforeEach
