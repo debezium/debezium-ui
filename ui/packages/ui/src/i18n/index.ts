15204e3d1a64b0cc700290726e85fe87c85d4092
indraraj <indraraj14@gmail.com>
2023-05-22 13:15:34 +0530

    DBZ-6473: fix test

--- ui/packages/ui/src/i18n/index.ts
+++ ui/packages/ui/src/i18n/index.ts
@@ -2,6 +2,12 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    returnNull: false;
+  }
+}
+
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
