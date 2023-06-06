e304acfefbeb9529e31cbfab1fb36bf7fc5db4a7
Indra <indraraj14@gmail.com>
2023-06-06 12:10:05 +0530

    DBZ-6473: update cypress version and test cases (#737)
    * DBZ-6473: update cypress version and test cases
    
    * DBZ-6473: build fix
    
    * DBZ-6473: CI test update
    
    * DBZ-6473: CI test fix
    
    * DBZ-6373: update versions

--- ui/cypress/e2e/connectors.cy.ts
+++ ui/cypress/e2e/connectors.cy.ts
@@ -1,28 +1,23 @@
 /// <reference types="cypress" />
 describe("Connectors page", () => {
-  before(() => {
+  it("Run a happy flow to create a connector", () => {
     cy.visit("/");
-  });
-  it("should appear header logo", () => {
+
     cy.get(".pf-c-page__header")
       .find("img")
       .should("have.attr", "src")
       .should("include", "debezium_logo_300px");
-  });
-  it("Wait and check if able to connect backend and get connectors", () => {
+
     cy.intercept("/api/connectors/1").as("getConnector");
     cy.wait("@getConnector", { timeout: 50000 })
       .its("response.statusCode")
       .should("eq", 200);
-  });
-  it("Find create connector button and click", () => {
+
     cy.findByText("Create a connector", { timeout: 50000 }).click();
-  });
-  it("Selects connector type PostgreSQL and click next button", () => {
+
     cy.findByText("PostgreSQL database", { timeout: 50000 }).click();
     cy.findByRole("button", { name: "Next" }).click();
-  });
-  it("Fill the form to configure connector", () => {
+
     cy.get('input[name="connector&name"]', { timeout: 50000 }).type(
       "dbz-pg-conn"
     );
@@ -40,28 +35,39 @@ describe("Connectors page", () => {
       "postgres"
     );
     cy.findByRole("button", { name: "Validate" }).click();
-  });
-  it("Checks if the form is valid", () => {
+
     cy.findByText("The validation was successful.").should("exist");
-  });
-  it("Skips to review step", () => {
+
     cy.findByRole("button", { name: "Review and finish" }).click();
-  });
-  it("Finish to configure connector", () => {
+
     cy.findByRole("button", { name: "Finish" }).click();
+
+    cy.wait(10000);
+
+    cy.findByText("dbz-pg-conn").should("exist");
   });
-  it("Connector action", () => {
+
+  it("Delete connector", () => {
+    cy.visit("/");
+
+    cy.get(".pf-c-page__header")
+      .find("img")
+      .should("have.attr", "src")
+      .should("include", "debezium_logo_300px");
+
+    cy.intercept("/api/connectors/1").as("getConnector");
+    cy.wait("@getConnector", { timeout: 50000 })
+      .its("response.statusCode")
+      .should("eq", 200);
+
     cy.get("tbody>tr", { timeout: 50000 })
       .eq(0)
       .find("td")
       .eq(5)
       .find(".pf-c-dropdown__toggle.pf-m-plain")
       .click();
-  });
-  it("Delete connector", () => {
+
     cy.get("ul>li").eq(7).scrollIntoView().findByText("Delete").click();
-  });
-  it("Confirm delete connector", () => {
     cy.get("body", { timeout: 50000 })
       .find(".pf-c-backdrop")
       .findByRole("button", { name: "Delete" })
